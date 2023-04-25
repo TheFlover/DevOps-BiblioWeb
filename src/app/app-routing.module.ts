e081ef7e08be06022f4245d853cfa8d1e6728e46
COULANGE Florian <59877487+TheFlover@users.noreply.github.com>
2023-04-25 16:23:45 +0200

    feat: 🎸 Ajout du routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BooksComponent } from './books/books.component';
+import { BookDetailComponent } from './book-detail/book-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'books', component: BooksComponent },
+  { path: 'books/:id', component: BookDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
